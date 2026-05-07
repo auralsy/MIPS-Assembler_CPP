cf19cea07e675979f08ff8d37451f5a6492b20a6
aura_230 <147060267+auralsy@users.noreply.github.com>
2026-05-07 19:27:04 +0900

    Refactor comments and add new instructions in input.asm
    Updated comments and modified instructions for clarity.

--- input.asm
+++ input.asm
@@ -1,17 +1,25 @@
-# 1. I-format & Register Initializing
+# Test Case
+
+# 1. 
 addi $t0, $zero, 5     
 addi $t1, $zero, 10    
 
-# 2. R-format & Arithmetic
-add $t2, $t0, $t1      
-sub $t3, $t1, $t0      
+# 2. 
+add $t2, $t0, $t1     
+sub $s0, $t1, $t0      
 
-# 3. Memory Access (I-format)
-sw $t2, 0($sp)        
-lw $s0, 0($sp)        
+# 3.
+and $t3, $t0, $t1      
+sll $t4, $t0, 2        
 
-# 4. Jump & Branch (J/I-format)
-beq $t2, $t2, target   
-target:
-j exit                
-exit:
+# 4.
+sw $t2, 0($sp)         
+lw $s1, 0($sp)         
+
+# 5.
+loop:                  
+addi $s0, $s0, -1     
+bne $s0, $zero, loop   
+
+j exit                 
+exit:                  
